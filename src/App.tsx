dc47c21fcd328c1e4cd561af8eda4160bc5ecab1
Luiz F Dias <luizfd@gmail.com>
2023-09-18 21:20:47 -0300

    Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/footer"
 import Header from "./components/header"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,18 @@
+import "./style.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="dsct-container">
+        <div className="dsct-container-footer">
+          <div>
+            <h1 className="dsct-arroba">@dscarrostop</h1>
+          </div>
+          <div>
+            <h1 className="dsct-endereco"><i>Rua da Pipoca, 150</i></h1>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
